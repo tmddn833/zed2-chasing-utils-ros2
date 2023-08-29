f23bcc05cad7323cdd837f49bff9dcc854f70dbd
larr-planning <yunwoo333@gmail.com>
2023-08-29 12:49:34 +0900

    Add custom types of data

--- include/zed2_chasing_utils/zed2_chasing_info_manager/zed2_chasing_info_manager.h
+++ include/zed2_chasing_utils/zed2_chasing_info_manager/zed2_chasing_info_manager.h
@@ -5,15 +5,14 @@
 #ifndef ZED2_CHASING_UTILS_ZED2_CHASING_INFO_MANAGER_H
 #define ZED2_CHASING_UTILS_ZED2_CHASING_INFO_MANAGER_H
 
-#include "image_transport/image_transport.hpp"
 #include "image_geometry/pinhole_camera_model.h"
-#include "sensor_msgs/msg/image.hpp"
+#include "image_transport/image_transport.hpp"
 #include "sensor_msgs/msg/compressed_image.hpp"
+#include "sensor_msgs/msg/image.hpp"
 
 #include "tf2_ros/transform_broadcaster.h"
 #include "tf2_ros/transform_listener.h"
-//#include "pcl_conversions/pcl_conversions.h"
-
+// #include "pcl_conversions/pcl_conversions.h"
 
 #include "compressed_depth_image_transport/codec.h"
 #include "compressed_depth_image_transport/compression_common.h"
@@ -21,28 +20,99 @@
 
 #include "zed_interfaces/msg/objects_stamped.hpp"
 
+#include "algorithm"
 #include "cv_bridge/cv_bridge.h"
-#include "opencv2/opencv.hpp"
+#include "eigen3/Eigen/Core"
+#include "eigen3/Eigen/Eigenvalues"
+#include "numeric"
 #include "opencv2/core/core.hpp"
 #include "opencv2/highgui/highgui.hpp"
-#include "algorithm"
-#include "numeric"
+#include "opencv2/opencv.hpp"
 
 namespace zed2_chasing_utils {
-    struct Param {
-        std::string global_frame_id;
-        int pcl_stride;
-        int mask_padding_x;
-        int mask_padding_y;
-    };
-    class ChasingInfoManager {
-    private:
-        Param param_;
-    public:
-        ChasingInfoManager();
-        void SetParameter(const std::string &global_frame_id, const int &mask_padding_x, const int &mask_padding_y);
-    };
-}
 
+struct Point {
+  float x;
+  float y;
+  float z;
 
-#endif //ZED2_CHASING_UTILS_ZED2_CHASING_INFO_MANAGER_H
+  Point() {
+    x = 0.0;
+    y = 0.0, z = 0.0;
+  };
+
+  Point(float x_, float y_, float z_) : x(x_), y(y_), z(z_){};
+
+  Point(const Eigen::Vector3f &pnt) : x(pnt(0)), y(pnt(1)), z(pnt(2)){};
+
+  Eigen::Vector3f toEigen() const { return Eigen::Vector3f(x, y, z); }
+
+  Eigen::Vector3d toEigend() const { return Eigen::Vector3d(x, y, z); }
+
+  Point operator+(const Point &pnt) const { return Point(pnt.x + x, pnt.y + y, pnt.z + z); }
+
+  Point operator-(const Point &pnt) const { return Point(x - pnt.x, y - pnt.y, z - pnt.z); }
+
+  Point operator*(float s) const { return Point(s * x, s * y, s * z); }
+
+  Point operator/(float s) const { return *this * (float)(1.0 / s); }
+
+  float distTo(Point p) const {
+    return (float)sqrt(pow(p.x - x, 2) + pow(p.y - y, 2) + pow(p.z - z, 2));
+  }
+
+  float dot(Point p) const { return x * p.x + y * p.y + z * p.z; }
+
+  float norm() const { return (float)sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2)); }
+
+  Point normalized() const { return *this * (float)(1.0 / this->norm()); }
+};
+
+struct Pose {
+  Eigen::Transform<float, 3, Eigen::Affine> poseMat;
+
+  void inverse() { poseMat = poseMat.inverse(); };
+
+  Pose() { poseMat.setIdentity(); }
+
+  void setTranslation(const float &x, const float &y, const float &z) {
+    poseMat.translate(Eigen::Vector3f(x, y, z));
+  };
+
+  void setTranslation(const Point &pnt) { poseMat.translate(pnt.toEigen()); };
+
+  void setRotation(const Eigen::Quaternionf &quat) { poseMat.rotate(quat); };
+
+  Point getTranslation() const {
+    return Point(poseMat.translation().x(), poseMat.translation().y(), poseMat.translation().z());
+  }
+
+  Eigen::Quaternionf getQuaternion() const { return Eigen::Quaternionf(poseMat.rotation()); }
+
+  void rotate(const Eigen::Vector3f &axis, const float &angle) {
+    poseMat.rotate(Eigen::AngleAxisf(angle, axis));
+  }
+
+  void applyTransform(const Pose &pose) { poseMat = pose.poseMat * poseMat; }
+};
+
+struct Param {
+  std::string global_frame_id;
+  int pcl_stride;
+  int mask_padding_x;
+  int mask_padding_y;
+};
+
+class ChasingInfoManager {
+private:
+  Param param_;
+
+public:
+  ChasingInfoManager();
+
+  void SetParameter(const std::string &global_frame_id, const int &mask_padding_x,
+                    const int &mask_padding_y);
+};
+} // namespace zed2_chasing_utils
+
+#endif // ZED2_CHASING_UTILS_ZED2_CHASING_INFO_MANAGER_H
